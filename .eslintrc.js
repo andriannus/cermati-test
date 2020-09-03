c019e5d88d781ae8401c51f2689f38af03dec5ae
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-09-04 00:29:40 +0700

    chore: update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/airbnb'
-  ],
+  extends: ["plugin:vue/essential", "@vue/airbnb"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint"
   },
   rules: {
+    "import/prefer-default-export": "off",
     "linebreak-style": 0,
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
   }
-}
+};
